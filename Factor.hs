e9d859433f195841ae87c9362cdca4bc0292469b
Vladimir Korolyov <balodja@gmail.com>
2014-01-02 14:35:46 +0400

    Two main functions for network construction.

--- Factor.hs
+++ Factor.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import qualified Data.Vector as V
-import qualified Data.IntMap as IM
+
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+
+import Control.Monad.State
 
 type Variable = Int
 
@@ -10,13 +16,24 @@ data Factor = Factor {
 
 data VariableDescription = VariableDescription {
     vardescDim :: !Int
-  , vardescName :: String
-  , vardescValues :: [String]
+--  , vardescName :: String
+--  , vardescValues :: [String]
   } deriving (Eq, Show, Read)
 
 data BayesNetwork = BayesNetwork {
-    bnetVariables :: IM.IntMap VariableDescription
+    bnetVariables :: IntMap VariableDescription
   , bnetFactors :: [Factor]
   } deriving (Eq, Show, Read)
+
+
+newVariable :: MonadState BayesNetwork m => VariableDescription -> m Variable
+newVariable desc = state modifyNet
+  where
+    modifyNet net = let variables = bnetVariables net
+                        var = succ . fst . IntMap.findMax $ variables
+                    in (var, net { bnetVariables = IntMap.insert var desc variables })
+
+addFactor :: MonadState BayesNetwork m => Factor -> m ()
+addFactor f = state $ \net -> ((), net { bnetFactors = f : bnetFactors net })
 
 main = putStrLn "halo"
